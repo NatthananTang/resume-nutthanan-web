8b8efc25f68236191385ad5b359376df956b8a94
Natthanan <natthanan@generwiz.com>
2022-12-19 14:26:20 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="container-detail">
+      <Row>
+        <Col xs={12} md={6} className="text-center">
+          <Image src={"photo-profire.jpg"} roundedCircle width={200} />
+        </Col>
+        <Col xs={12} md={6}>
+          <Row>
+            <Col xs={12} md={12}>
+              <label>
+                Back-end devaloper. Experience in C# web devalopment.
+                understanding the basic html, css and jquery
+              </label>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} md={6} className="text-center">
+          <h3>Nutthanan Yaonopas</h3>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
