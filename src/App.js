7f1cc93b2ac03e46a5d7983ee75edac67706561d
Natthanan <natthanan@generwiz.com>
2022-12-19 14:57:47 +0700

    update

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 
+// Icon
+import { BsFillPinMapFill } from "react-icons/bs";
+
 function App() {
   return (
     <Container className="container-detail">
       <Row>
-        <Col xs={12} md={6} className="text-center">
+        <Col xs={12} md={6}>
           <Image src={"photo-profire.jpg"} roundedCircle width={200} />
         </Col>
         <Col xs={12} md={6}>
@@ -22,8 +25,56 @@ function App() {
         </Col>
       </Row>
       <Row>
-        <Col xs={12} md={6} className="text-center">
+        <Col xs={12} md={6}>
           <h3>Nutthanan Yaonopas</h3>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} md={6}>
+          <label>Programmer</label>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} md={6}>
+          <BsFillPinMapFill></BsFillPinMapFill> <label>Bangkok, Bung Sue</label>
+        </Col>
+      </Row>
+
+      <hr></hr>
+
+      <Row>
+        <Col xs={12} md={12}>
+          <h4>Skills</h4>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} md={12}>
+          <label>- Html</label>
+          <br></br>
+          <label>- Bootstrap</label>
+          <br></br>
+          <label>- Css</label>
+          <br></br>
+          <label>- Jquery</label>
+          <br></br>
+          <label>- React.js</label>
+          <br></br>
+          <label>- C# .NET Core</label>
+          <br></br>
+          <label>- Sql (sql server)</label>
+        </Col>
+      </Row>
+
+      <hr></hr>
+
+      <Row>
+        <Col xs={12} md={12}>
+          <h4>Experience</h4>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} md={12}>
+        <label>3 years working experience in c# development in Generwiz Co., Ltd. company</label>
         </Col>
       </Row>
     </Container>
