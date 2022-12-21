1aba2a4986ebe1eb0fb58a37eb9f969bb440e299
Natthanan <natthanan@generwiz.com>
2022-12-21 14:40:32 +0700

    update

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom/client";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css";
 import App from "./App";
+import About from "./About";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-  },
+  }, 
+  {
+    path: "/About",
+    element: <About />,
+  }
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
